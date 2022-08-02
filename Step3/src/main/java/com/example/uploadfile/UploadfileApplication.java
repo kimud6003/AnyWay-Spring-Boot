b5cbf4edfd872e0560608ac1a17e095847c3f379
root <root@KUD.localdomain>
2022-08-02 21:21:11 +0900

    Step3 complete

--- Step3/src/main/java/com/example/uploadfile/UploadfileApplication.java
+++ Step3/src/main/java/com/example/uploadfile/UploadfileApplication.java
@@ -1,13 +1,27 @@
 package com.example.uploadfile;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import com.example.uploadfile.storage.StorageProperties;
+import com.example.uploadfile.storage.StorageService;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class UploadfileApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UploadfileApplication.class, args);
+	}
+	@Bean
+	CommandLineRunner init(StorageService storageService){
+		return (args)->{
+			storageService.deleteAll();
+			storageService.init();
+		};
 	}
 
 }
